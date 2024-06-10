e6cc70605524bd4d0f23e2776de46904629253a8
hengyulee <hengyuli901229@gmail.com>
2024-06-10 23:07:01 +0800

    rewrite filtering

--- sorter.js
+++ sorter.js
@@ -1,14 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
     sortSideBar();
+    sortFrontPageCourseList();
 });
 
 function sortSideBar() {
     const sidebarList = document.querySelector('.unlist');
-    const links = Array.from(sidebarList.querySelectorAll('a.dimmed'));
-    const nonDimmedLinks = Array.from(sidebarList.querySelectorAll(':not(a.dimmed)')).filter(link => link.tagName === 'A');
+    const banned_prefix = ["AASO", "BSC", "ECON", "CS_", "CC"];
+    const nonDimmedLinks = Array.from(sidebarList.querySelectorAll('a:not(.dimmed)'))
+        .filter(link =>
+            link.tagName === 'A' &&
+            !banned_prefix.some(prefix => link.textContent.startsWith(prefix))
+        );
+
     nonDimmedLinks.sort((a, b) => a.textContent.localeCompare(b.textContent));
-    const sortedLinks = nonDimmedLinks.concat(links);
     sidebarList.innerHTML = '';
-    sortedLinks.forEach(link => sidebarList.appendChild(link.parentNode));
+    nonDimmedLinks.forEach(link => sidebarList.appendChild(link.parentNode));
 }
 
+function sortFrontPageCourseList() {
+    const courseList = document.querySelector('.frontpage-course-list-enrolled');
+    const courseBoxes = Array.from(courseList.querySelectorAll('.coursebox'));
+    if (courseBoxes.length == 0) {
+        const sortedCourseBoxes = courseBoxes.sort((a, b) => {
+            const textA = a.querySelector('h3.coursename').textContent.toLowerCase();
+            const textB = b.querySelector('h3.coursename').textContent.toLowerCase();
+            if (textA < textB) {
+                return -1;
+            }
+            if (textA > textB) {
+                return 1;
+            }
+            return 0;
+        });
+        courseList.innerHTML = '';
+        sortedCourseBoxes.forEach(courseBox => courseList.appendChild(courseBox));
+    }
+};
