ef2e66fdd3750599e313ea45696d57e8dc69db4b
hengyulee <hengyuli901229@gmail.com>
2024-11-27 12:07:41 +0900

    Check if tab is under the course page for course id

--- moodle-sort/src/browser/tab.ts
+++ moodle-sort/src/browser/tab.ts
@@ -4,7 +4,11 @@ export const getCourseIdFromBroswer = () => {
       const activeTab = tabs[0];
       if (activeTab?.url) {
         const url = new URL(activeTab.url);
-        if (url.hostname !== "moodle.hku.hk") resolve(-1);
+        if (
+          url.hostname !== "moodle.hku.hk" ||
+          !url.pathname.startsWith("/course/")
+        )
+          resolve(-1);
         const idParam = url.searchParams.get("id");
         const parsedId = idParam ? Number(idParam) : -1;
         resolve(!isNaN(parsedId) ? parsedId : -1);
